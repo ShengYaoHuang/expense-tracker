3db83e5b22ca2e4442e79938a0dc4b90df543559
Yao <capslock0921@gmail.com>
2020-08-03 22:25:59 +0800

    test run app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+
+
+app.listen(3000, () => {
+  console.log('Express is running on http://localhost:3000')
+})
