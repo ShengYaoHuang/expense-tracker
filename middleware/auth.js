bc4c6e53f2bc8f12db94c30d4aa234302822059b
Yao <capslock0921@gmail.com>
2020-08-24 21:35:20 +0800

    feat: add user authentication in record and home routers

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+const { model } = require("../models/user");
+
+module.exports = {
+  authenticator: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/users/login')
+  }
+}
